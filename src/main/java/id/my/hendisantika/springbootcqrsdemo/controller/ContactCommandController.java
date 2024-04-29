3ef2713d9d225e0b3638de0d3756ee2a73818205
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 06:39:08 +0700

    Add create in ContactCommandController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcqrsdemo/controller/ContactCommandController.java
+++ src/main/java/id/my/hendisantika/springbootcqrsdemo/controller/ContactCommandController.java
@@ -1,10 +1,18 @@
 package id.my.hendisantika.springbootcqrsdemo.controller;
 
+import id.my.hendisantika.springbootcqrsdemo.dto.ContactCommandDto;
+import id.my.hendisantika.springbootcqrsdemo.model.Contact;
 import id.my.hendisantika.springbootcqrsdemo.service.ContactCommandService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +30,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ContactCommandController {
     private final ContactCommandService contactCommandService;
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody ContactCommandDto contactDto) {
+        Contact contact = new Contact(contactDto.getId(), contactDto.getName(), contactDto.getEmail(), contactDto.getPhone());
+        long savedContactId = contactCommandService.create(contact);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedContactId).toUri();
+        return ResponseEntity.created(location).build();
+    }
 }
