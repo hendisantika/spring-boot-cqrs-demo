ff47e52abf53716a362d4b4d574abcaa74b35cd5
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 06:36:05 +0700

    Add ContactCommandService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootcqrsdemo/service/ContactCommandService.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.springbootcqrsdemo.service;
+
+import id.my.hendisantika.springbootcqrsdemo.repository.ContactRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cqrs-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/30/24
+ * Time: 06:35
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class ContactCommandService {
+    private final ContactRepository contactRepository;
+}
