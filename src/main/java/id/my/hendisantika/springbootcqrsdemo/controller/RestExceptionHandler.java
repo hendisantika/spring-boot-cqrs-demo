f9233823aac9271d827d54828d34caf22492ed55
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 06:40:03 +0700

    Add RestExceptionHandler 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootcqrsdemo/controller/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.springbootcqrsdemo.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cqrs-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/30/24
+ * Time: 06:39
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+}
