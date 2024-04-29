9ea534927aaef46754069fceeb7d564774e322a8
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 06:32:35 +0700

    Add Contact Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootcqrsdemo/model/Contact.java
@@ -0,0 +1,108 @@
+package id.my.hendisantika.springbootcqrsdemo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
+
+import java.util.Objects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cqrs-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/30/24
+ * Time: 06:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@SequenceGenerator(name = "contactSeq", sequenceName = "s_contact", allocationSize = 1)
+public class Contact {
+
+    private long id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    public Contact() {
+    }
+
+    public Contact(long id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "contactSeq")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+
+        if (id != contact.id) return false;
+        if (!Objects.equals(name, contact.name)) return false;
+        if (!Objects.equals(email, contact.email)) return false;
+        return Objects.equals(phone, contact.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
