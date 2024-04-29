8e1ad0b95c8d55a1377c155b3baf40e299ab15c9
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 06:42:00 +0700

    Add getContacts in ContactQueryController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcqrsdemo/controller/ContactQueryController.java
+++ src/main/java/id/my/hendisantika/springbootcqrsdemo/controller/ContactQueryController.java
@@ -1,13 +1,18 @@
 package id.my.hendisantika.springbootcqrsdemo.controller;
 
+import id.my.hendisantika.springbootcqrsdemo.dto.ContactQueryDto;
 import id.my.hendisantika.springbootcqrsdemo.model.Contact;
 import id.my.hendisantika.springbootcqrsdemo.service.ContactQueryService;
+import jakarta.persistence.NoResultException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +36,12 @@ public class ContactQueryController {
         return contactQueryService
                 .findById(id)
                 .orElseThrow(NoResultException::new);
+    }
+
+    @GetMapping
+    public List<ContactQueryDto> getContacts() {
+        return contactQueryService.findAll().stream()
+                .map(con -> new ContactQueryDto(con.getId(), con.getName(), con.getEmail(), con.getPhone()))
+                .collect(Collectors.toList());
     }
 }
