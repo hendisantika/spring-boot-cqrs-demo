3dc2d0f84eea8ef4b4f26696a8b74977347a6d48
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 06:36:40 +0700

    Add update in ContactCommandService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcqrsdemo/service/ContactCommandService.java
+++ src/main/java/id/my/hendisantika/springbootcqrsdemo/service/ContactCommandService.java
@@ -27,4 +27,8 @@ public class ContactCommandService {
     public Long create(Contact contact) {
         return contactRepository.create(contact);
     }
+
+    public void update(Contact contact) {
+        contactRepository.update(contact);
+    }
 }
