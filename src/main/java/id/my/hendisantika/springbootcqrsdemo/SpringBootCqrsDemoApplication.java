422fe5f1852e1d2a106cc413d9bab73110c6bdd3
hendisantika <hendisantika@yahoo.co.id>
2024-04-30 06:26:11 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootcqrsdemo/SpringBootCqrsDemoApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootcqrsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCqrsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCqrsDemoApplication.class, args);
+    }
+
+}
